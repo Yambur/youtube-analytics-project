c7ff1bef4b00c55fc1c4769f9b4123b196d2f578
bargest <DIZz92@mail.ru>
2023-10-31 08:13:11 +0300

    hz

--- src/video.py
+++ src/video.py
@@ -9,8 +9,8 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
+                                                    id=video_id
+                                                    ).execute()
 
         self.video_title: str = video_response['items'][0]['snippet']['title']
         self.view_count: int = video_response['items'][0]['statistics']['viewCount']
@@ -21,57 +21,7 @@ class Video:
         return self.video_title
 
 
-
 class PLVideo(Video):
     def __init__(self, video_id, playlist_id):
         super().__init__(video_id)
         self.playlist_id = playlist_id
-
-
-
-
-
-
-
-
-
-
-
-"""class Video(Channel):
-    def __init__(self, video_id):
-        youtube = super().get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.video_response = video_response
-        self.__video_id = video_id
-        self.url = f'https://youtu.be/{self.video_id}'
-        try:
-            self.title = video_response['items'][0]['snippet']['title']
-            self.description = video_response['items'][0]['snippet']['description']
-            self.like_count = video_response['items'][0]['statistics']['likeCount']
-            self.view = video_response['items'][0]['statistics']['viewCount']
-        except IndexError:
-            self.title = None
-            self.description = None
-            self.like_count = None
-            self.view = None
-
-    def __str__(self):
-        return f"{self.title}"
-
-    @property
-    def video_id(self):
-        return self.__video_id
-
-
-class PLVideo(Video):
-
-    def __init__(self, video_id, playlist_id):
-        super().__init__(video_id)
-        youtube = super().get_service()
-        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
-                                                       part='contentDetails, snippet',
-                                                       maxResults=50,
-                                                       ).execute()
-        self.playlist_id = [video['contentDetails']['videoId'] for video in playlist_videos['items']]"""

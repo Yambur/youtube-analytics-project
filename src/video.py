fee214ccd5ebc23f6986225bcba0171294283fea
bargest <DIZz92@mail.ru>
2023-11-06 14:20:35 +0300

    add try and except

--- src/video.py
+++ src/video.py
@@ -9,13 +9,18 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=video_id
-                                                    ).execute()
-
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+                                                        id=video_id
+                                                        ).execute()
+        try:
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.video_title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return self.video_title
